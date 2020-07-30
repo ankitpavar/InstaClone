9c4536f63d001aa5e61e67f3a6dce70d65771bd7
Ankit Pavar <geekyanky@gmail.com>
2020-07-30 20:33:38 +0530

    added logo in navbar

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,10 +1,23 @@
-import React from "react";
-import { useLayoutStyles } from "../../styles";
+import React from 'react';
+import { useLayoutStyles } from '../../styles';
+import SEO from '../shared/Seo'
+import Navbar from '../shared/Navbar';
 
-function Layout() {
-  useLayoutStyles();
 
-  return <div>Layout</div>;
+function Layout({ children, title }) {
+  const classes = useLayoutStyles();
+
+  return (
+    <section className={classes.section}>
+      <SEO title={title}/>
+      <Navbar/>
+      <main className={classes.main}>
+        <section className={classes.childrenWrapper}>
+          <div className={classes.children}>{children}</div>
+        </section>
+      </main>
+    </section>
+  );
 }
 
 export default Layout;
--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,7 +1,7 @@
 import React from "react";
-
+import Layout from "../components/shared/Layout"; 
 function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  return <Layout title="Page Not Found">NotFoundPage</Layout>;
 }
 
 export default NotFoundPage;
