b55d415f1c10437b646f45e50ee2e1bcc0636835
Ankit Pavar <geekyanky@gmail.com>
2020-07-31 14:31:06 +0530

    feed page card initial design done

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { useFeedPageStyles } from '../styles';
 import Layout from '../components/shared/Layout';
-import { defaultPost } from '../data'
+import UserCard from '../components/shared/UserCard';
+import { getDefaultPost } from '../data'
+import FeedPost from '../components/feed/FeedPost'
+import FeedSideSuggestions from '../components/feed/FeedSideSuggestions'
+import { Hidden } from '@material-ui/core';
+
 function FeedPage() {
   const classes = useFeedPageStyles();
 
   return (
     <Layout>
       <div className={classes.container}>
+        {/* FeedPost */}
         <div>
-        
+        {Array.from({ length: 5}, () => getDefaultPost()).map(post => (
+          <FeedPost key={post.id} post={post}/>
+        ))}
         </div>
+        {/* Sidebar */}
+        <Hidden smDown>
+          <div className={classes.sidebarContainer}>
+            <div className={classes.sidebarWrapper}>
+              <UserCard/>
+              <FeedSideSuggestions/>
+            </div>
+          </div>
+        </Hidden>
       </div>
     </Layout>
   );
